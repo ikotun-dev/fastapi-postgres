86cdd2d927960a71f694aad0df303d7856dc71d8
Ikotun Collins <danlogan2003@gmail.com>
2023-06-04 17:05:16 +0100

    made the biker model proper

--- TEST/schemas.py
+++ TEST/schemas.py
@@ -5,23 +5,23 @@ from datetime import datetime
 class Biker(BaseModel):
     id: Optional[int]
     username: str = Field(..., max_length=255, unique=True)
-    # firstname: str = Field(None, max_length=255)
-    # lastname: str = Field(None, max_length=255)
-    password: str
-    # phone_number: int = 0
-    # email: str = None
-    # profile_image:  str 
-    # created_at: datetime =  Field(default_factory=datetime.now())
-    # club: bool = False
-    # badge: bool = False
-    # location: str = None
-    # about_me: str = None
-    # following: List[int] = []
-    # followers_count: int = 0
-    # following_count: int = 0 
-    # max_speed: int = None
-    # confirmation_code: int = None
-    # verification_status: bool = False
+    firstname: Optional[str] = Field(max_length=255)
+    lastname: Optional[str] = Field(max_length=255)
+    password: str = Field(..., max_length=255)
+    phone_number: Optional[int] 
+    email: Optional[str]
+    profile_image:  Optional[str] 
+    created_at: Optional[datetime] =  Field(default_factory=datetime.now)
+    club: Optional[bool] = False
+    badge: Optional[bool] = False
+    location: Optional[str] = None
+    about_me: Optional[str] = None
+    following: Optional[List[int]] = []
+    followers_count: Optional[int] = 0
+    following_count: Optional[int] = 0 
+    max_speed: Optional[int] = None
+    confirmation_code: Optional[int] = None
+    verification_status: Optional[bool] = False
 
 
 
