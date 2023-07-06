5320c516cf3db5c050048c9f388c426213298002
Ikotun Collins <danlogan2003@gmail.com>
2023-07-06 12:26:33 +0100

    crud db

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Response
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
 class Post(BaseModel):
     title : str
-    description : str
+ #   description : str
 
 
 app = FastAPI()
@@ -21,17 +21,48 @@ except Exception as e:
     print(e)
 
     
-@app.get("/theposts")
+@app.get("/posts")
 def get_posts():
     cursor.execute(""" SELECT * from posts """)
     posts = cursor.fetchall()
     print(posts)
     return {'data' : posts}
 
-@app.get("/posts")
-def create_post():
-    posts = cursor.execute(""" SELECT * from posts """)
-    print(posts)
-    return {'data' : posts}
+@app.post("/posts")
+def create_post(post: Post):
+    cursor.execute(""" INSERT INTO posts (title) VALUES (%s) RETURNING *""", (post.title,))
+    new_post = cursor.fetchone()
 
+    conn.commit()
+
+    return {'data' : new_post}
+
+@app.get("/posts/{id}")
+def get_posts(id: str):
+    cursor.execute(""" SELECT * from posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
+    if not post : 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    return {'data' : post}
+
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    cursor.execute("""DELETE FROM posts WHERE id  = %s RETURNING * """, (str(id),))
+    deleted_post =  cursor.fetchone()
+    if deleted_post == None :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")  
+def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s  WHERE id = %s RETURNING *""",  (post.title, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    
+
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return {'data' : updated_post}
 
